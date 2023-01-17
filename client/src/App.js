fc6df9979709633a25b1882e88748a61f408ceac
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2023-01-16 22:34:38 -0800

    yay

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>buttholes</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        peepeepoopoo
       </header>
     </div>
   );
