8da49a5ab9e3e2ac0e24ce29746e787c00b0bca7
Darryl Tanzil <darryltanzil@gmail.com>
2023-01-21 13:57:20 -0800

    update: checks to see if nodes in frame before starting line, UI changes to canvas

--- client/src/components/Nodes.js
+++ client/src/components/Nodes.js
@@ -2,9 +2,12 @@ import { Pose } from '@tensorflow-models/pose-detection';
 
 
 const Nodes = (ctx, pose) => {
-    ctx.fillStyle = 'red';
-    ctx.strokeStyle = 'red';
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'white';
+    
     ctx.lineWidth = 5;
+    const facePoints = ['nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear'];
+    const scoreThreshold = 0.9;
 
     function drawLine(ctx, pointA, pointB) {
         if (pointA && pointB) { 
@@ -18,40 +21,61 @@ const Nodes = (ctx, pose) => {
 
     if (ctx) {
         const points = new Map();
+        pose.keypoints.map(point => points.set(point.isDrawn, false));
         pose.keypoints.map(point => points.set(point.name, point));
         pose.keypoints.forEach(point => drawPoint(ctx, point, false, true));
         
-        drawLine(ctx, points.get('left_shoulder'), points.get('right_shoulder'));
-        drawLine(ctx, points.get('left_hip'), points.get('right_hip'));
-    
-        // left arm
-        drawLine(ctx, points.get('left_shoulder'), points.get('left_elbow'));
-        drawLine(ctx, points.get('left_elbow'), points.get('left_wrist'));
-        
+        if (points.get('left_shoulder').isDrawn&& points.get('right_shoulder').isDrawn) {
+            drawLine(ctx, points.get('left_shoulder'), points.get('right_shoulder'));
+        }
+        if (points.get('left_hip').isDrawn&& points.get('right_hip').isDrawn) {
+            drawLine(ctx, points.get('left_hip'), points.get('right_hip'));
+        }
+        if (points.get('left_shoulder').isDrawn && points.get('left_elbow').isDrawn) {
+            drawLine(ctx, points.get('left_shoulder'), points.get('left_elbow'));
+        }
+        if (points.get('left_elbow').isDrawn && points.get('left_wrist').isDrawn) {
+            drawLine(ctx, points.get('left_elbow'), points.get('left_wrist'));
+        }
         // left side
-        drawLine(ctx, points.get('left_shoulder'), points.get('left_hip'));
-        
+        if (points.get('left_shoulder').isDrawn && points.get('left_hip').isDrawn) {
+            drawLine(ctx, points.get('left_shoulder'), points.get('left_hip'));
+        }
         // left leg
+        if (points.get('left_hip').isDrawn && points.get('left_knee').isDrawn) {
         drawLine(ctx, points.get('left_hip'), points.get('left_knee'));
-        drawLine(ctx, points.get('left_knee'), points.get('left_ankle'));
+        }
 
+        if (points.get('left_knee').isDrawn && points.get('left_ankle').isDrawn) {
+            drawLine(ctx, points.get('left_knee'), points.get('left_ankle'));
+        }
         // right arm
-        drawLine(ctx, points.get('right_shoulder'), points.get('right_elbow'));
-        drawLine(ctx, points.get('right_elbow'), points.get('right_wrist'));
-
-        // right side
-        drawLine(ctx, points.get('right_shoulder'), points.get('right_hip'));
+        if (points.get('right_shoulder').isDrawn && points.get('right_elbow').isDrawn) {
+            drawLine(ctx, points.get('right_shoulder'), points.get('right_elbow'));
+        }
+        if (points.get('right_elbow').isDrawn && points.get('right_wrist').isDrawn) {
+            drawLine(ctx, points.get('right_elbow'), points.get('right_wrist'));
+        }
+        if (points.get('right_shoulder').isDrawn && points.get('right_hip').isDrawn) {
+            drawLine(ctx, points.get('right_shoulder'), points.get('right_hip'));
+        }
         
         // right leg
         drawLine(ctx, points.get('right_hip'), points.get('right_knee'));
         drawLine(ctx, points.get('right_knee'), points.get('right_ankle'));
 
         function drawPoint(ctx, point, showLabels, showFacePoints) {
-            if (showFacePoints) {
+            if (point.score >= scoreThreshold &&
+                 (!facePoints.includes(point.name))) {
+                point.isDrawn = true;
+                ctx.fillStyle = '#74C5A3';
+                ctx.strokeStyle = '#74C5A3';
                 ctx.beginPath();
-                ctx.arc(point.x, point.y, 2, 0, 2 * Math.PI);
+                ctx.arc(point.x, point.y, 10, 0, 2 * Math.PI);
                 ctx.stroke();
                 ctx.closePath();
+                ctx.fillStyle = 'white';
+                ctx.strokeStyle = 'white';
                 if (showLabels) {
                     ctx.fillText(point.name, point.x, point.y);
                 }
