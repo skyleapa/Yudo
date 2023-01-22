1b0313a858cbec395abfb3d484322eb33967e0db
Michelle Wang <54217965+skyleapa@users.noreply.github.com>
2023-01-21 22:14:02 -0800

    workoutlist

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Exercises from "./components/ui/Exercises";
 import Workouts from "./components/ui/Workouts";
 import NavigationBar from "./components/ui/NavigationBar";
 import LivePage from "./components/ui/LivePage";
+import WorkoutList from "./components/ui/WorkoutList";
 
 function App() {
   return (
@@ -13,10 +14,12 @@ function App() {
       <Router>
         <NavigationBar />
         <header className="App-header">
+          7777777
           <Routes>
             <Route exact path="/" element={<LandingPage />} />
             {/* <Route exact path="live" element={<LivePage />} */}
             <Route exact path="workouts" element={<Workouts />} />
+            <Route exact path="workoutlists" element={<WorkoutList />} />
             <Route exact path="exercises" element={<Exercises />} />
           </Routes>
         </header>
