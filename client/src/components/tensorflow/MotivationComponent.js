f9bbf986ba57cfa9b2f2825b0d8f8652ab3976d0
joshc4 <joshchan4444@gmail.com>
2023-01-22 09:27:53 -0800

    fixed timer

--- client/src/components/tensorflow/MotivationComponent.js
+++ client/src/components/tensorflow/MotivationComponent.js
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 const MotivationComponent = (parentScore, scoreArray) => {
     let [insultText, setInsultText] = useState(null);
     let [count, setCount] = useState(0);
+    const [secondsRemaining, setSecondsRemaining] = useState(30000)
     const navigate = useNavigate();
     const handleClick = () => navigate('/endpage');
     const getInsultOrComp = (theScore) => {
@@ -15,6 +16,11 @@ const MotivationComponent = (parentScore, scoreArray) => {
             setInsultText(result);
         });
     }
+
+    useEffect(() => {
+        const timerID = setInterval(() => setSecondsRemaining(prevSecondsRemaining => prevSecondsRemaining - 1000), 1000)
+        return () => clearInterval(timerID)
+    }) 
 
     function onFinish() {
         let sum = 0;
@@ -28,7 +34,7 @@ const MotivationComponent = (parentScore, scoreArray) => {
     return (
         <>
             <p>Current motivation: {insultText} </p>
-            Timer: <Countdown date={Date.now() + 30000} onComplete={() => {onFinish()}} />
+            Timer: <Countdown date={Date.now() + secondsRemaining} onComplete={() => {onFinish()}} />
             <button onClick={() => {
                 getInsultOrComp(parentScore)}}>
                 Get Insult
