d2c10382ef5b0c438ac14b0b82f5565bf4870cda
Darryl Tanzil <darryltanzil@gmail.com>
2023-01-21 16:12:45 -0800

    update: added images folder, working on nav bar, creating normalized data

--- client/src/components/ui/NavigationBar.js
+++ client/src/components/ui/NavigationBar.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import head from './images/head.png'; // with import
+
   
 export default function NavigationBar() {
     return (
         <div id="navigationBar">
-            <h1>Navigation Peepee Poopoo</h1>
+            <h1>Yudo</h1>
         </div>
     )
 }
