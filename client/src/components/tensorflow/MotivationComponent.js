d64ed2fd2fb654bbc532ccecae926464d1b95314
Darryl Tanzil <darryltanzil@gmail.com>
2023-01-22 08:51:19 -0800

    update: added router program nav

--- client/src/components/tensorflow/MotivationComponent.js
+++ client/src/components/tensorflow/MotivationComponent.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import React, { useState, useRef, useEffect } from 'react';
 import Countdown from 'react-countdown';
+import { useNavigate } from 'react-router-dom';
 
 const MotivationComponent = (parentScore) => {
     let [insultText, setInsultText] = useState(null);
     let [count, setCount] = useState(0);
-    
+    const navigate = useNavigate();
+    const handleClick = () => navigate('/endpage');
     const getInsultOrComp = (theScore) => {
         axios.post("http://localhost:5001/ai-response", theScore)
           .then((response) => {
@@ -17,7 +19,7 @@ const MotivationComponent = (parentScore) => {
     return (
         <>
             <p>Current motivation: {insultText} </p>
-            Timer: <Countdown date={Date.now() + 30000} onComplete={() => {getInsultOrComp(0.5)}} />
+            Timer: <Countdown date={Date.now() + 30000} onComplete={handleClick} />
             <button onClick={() => {
                 getInsultOrComp(parentScore)}}>
                 Get Insult
