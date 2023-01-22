2b208f0e7e1b5c4936a8a8af0b277b7e4558f26a
Darryl Tanzil <darryltanzil@gmail.com>
2023-01-21 19:35:43 -0800

    delete redundant

--- client/src/components/ui/LandingPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-  
-export default function LandingPage() {
-    return (
-        <div>
-            <h1>Test Landing Page</h1>
-        </div>
-    )
-}
--- client/src/components/ui/Workouts.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-  
-export default function Workouts() {
-    return (
-        <div>
-            <h1>Ppepeppepe</h1>
-        </div>
-    )
-}
